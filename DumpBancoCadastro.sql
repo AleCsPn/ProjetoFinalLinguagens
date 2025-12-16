75c19d0208fd45e02a45fb14b6a499799e1c7f50
AleCsPn <alexandrecostapenhasilva@gmail.com>
2025-12-16 17:40:21 -0300

    Refatoração do SQL

--- DumpBancoCadastro.sql
+++ DumpBancoCadastro.sql
@@ -1,73 +1,123 @@
-DROP TABLE IF EXISTS `generos`;
+DROP TABLE IF EXISTS `setor`;
 
-CREATE TABLE `generos` (
-  `idgenero` int NOT NULL AUTO_INCREMENT,
-  `genero` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idgenero`)
+CREATE TABLE `setor` (
+  `idsetor` int NOT NULL AUTO_INCREMENT,
+  `nomesetor` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idsetor`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-LOCK TABLES `generos` WRITE;
+LOCK TABLES `setor` WRITE;
 
-INSERT INTO `generos` VALUES (1,'Rock'),(2,'Metal'),(3,'Pagode'),(4,'Gospel'),(5,'Funk');
+INSERT INTO `setor` VALUES (6,'bebidas'),(7,'Limpeza'),(8,'Alimentos');
 
 UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `bandas`;
+DROP TABLE IF EXISTS `produtos`;
 
-CREATE TABLE `bandas` (
-  `idbandas` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `produtos` (
+  `idprodutos` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
-  `integrantes` int DEFAULT NULL,
-  `ranking` int DEFAULT NULL,
-  `fk_genero` int DEFAULT NULL,
-  PRIMARY KEY (`idbandas`),
-  KEY `genero_idx` (`fk_genero`),
-  CONSTRAINT `genero` FOREIGN KEY (`fk_genero`) REFERENCES `generos` (`idgenero`)
+  `quantidade` int DEFAULT NULL,
+  `preco` double DEFAULT NULL,
+  `fk_setor` int DEFAULT NULL,
+  PRIMARY KEY (`idprodutos`),
+  KEY `setor_idx` (`fk_setor`),
+  CONSTRAINT `setor` FOREIGN KEY (`fk_setor`) REFERENCES `setor` (`idsetor`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
-LOCK TABLES `bandas` WRITE;
+LOCK TABLES `produtos` WRITE;
 
-INSERT INTO `bandas` VALUES (1,'Skank',5,1,1),(2,'Metallica',4,1,1);
+INSERT INTO `produtos` VALUES (4,'Refri 660 ML',20,5.5,6),(7,'Água Sanitária',100,13.50,7);
 
 UNLOCK TABLES;
 
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_insereBanda`(nome varchar(45), 
-                                   integrantes int, ranking int, genero int)
-BEGIN
-INSERT INTO `bandas`
-(`nome`,
-`integrantes`,
-`ranking`,
-`fk_genero`)
-VALUES
-(nome,
-integrantes,
-ranking,
-genero);
-END ;;
-DELIMITER ;
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `idusuarios` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  PRIMARY KEY (`idusuarios`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `usuarios`: WRITE;
+
+INSERT INTO `usuarios` VALUES (3,'adm','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3'),(4,'user1','5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5');
 
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_listaBandas`()
+CREATE DEFINER="avnadmin"@"%" PROCEDURE "sp_listaProdutos"()
 BEGIN
-     SELECT 
-        b.idbandas, 
-        b.nome, 
-        b.ranking, 
-        g.genero
-    FROM bandas b
-    INNER JOIN generos g 
-        ON g.idgenero = b.fk_genero;
-END ;;
-DELIMITER ;
-
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_listaGeneros`()
-BEGIN
-   select * from generos;
+	select idprodutos,nome,quantidade,preco, setor.nomesetor
+    from produtos
+	INNER JOIN setor 
+			on produtos.fk_setor = idsetor;
+    
 END;;
 DELIMITER ;
+
+DELIMITER ;;
+CREATE DEFINER="avnadmin"@"%" PROCEDURE "sp_insereProduto"(nome varchar(45), quantidade int,preco double, setor int)
+BEGIN
+INSERT INTO produtos
+(
+`nome`,
+`quantidade`,
+`preco`,
+`fk_setor`)
+VALUES
+(
+nome,
+quantidade,
+preco,
+setor);
+
+END;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE DEFINER="avnadmin"@"%" PROCEDURE "sp_listaSetor"()
+BEGIN
+	select * from setor;
+END;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE DEFINER="avnadmin"@"%" PROCEDURE "sp_alteraProduto"(idproduto int, nome varchar(45), quantidade int, preco double, setor int)
+BEGIN
+UPDATE `produtos`
+SET
+`nome` = nome,
+`quantidade` = quantidade,
+`preco` = preco,
+`fk_setor` = setor
+WHERE `idprodutos` = idproduto;
+
+END;;
+DELIMITER;
+
+DELIMITER;;
+CREATE DEFINER="avnadmin"@"%" PROCEDURE "sp_consultaLogin"(usuario varchar(100), senha varchar(100))
+BEGIN
+Select * from usuarios where usuarios.nome = usuario and usuarios.senha = senha;
+END;;
+DELIMITER;
+
+DELIMITER;;
+CREATE DEFINER="avnadmin"@"%" PROCEDURE "sp_insereSetor"(nomeSetor varchar(45))
+BEGIN
+INSERT INTO `setor`
+(`nomesetor`)
+VALUES
+(nomeSetor);
+
+END;;
+DELIMITER;
+
+DELIMITER;;
+CREATE DEFINER="avnadmin"@"%" PROCEDURE "sp_removeProduto"(idproduto int)
+BEGIN
+	delete from produtos where idprodutos = idproduto;
+END;;
+DELIMITER;
